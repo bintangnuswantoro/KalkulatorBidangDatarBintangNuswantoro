a417b43507daacf2e5208f135eaafe2ec638eaa5
bintang <nuswantorobintang1@gmail.com>
2022-10-12 18:46:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kalkulatorbidangdatarbintangnuswantoro/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.kalkulatorbidangdatarbintangnuswantoro;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void persegi(View view) {
+        Intent intent = new Intent(MainActivity.this, persegi.class);
+        startActivity(intent);
+    }
+
+    public void persegi_panjang(View view) {
+        Intent intent = new Intent(MainActivity.this, persegipanjang.class);
+        startActivity(intent);
+    }
+
+    public void segitiga(View view) {
+        Intent intent = new Intent(MainActivity.this, segitiga.class);
+        startActivity(intent);
+    }
+
+    public void lingkaran(View view) {
+        Intent intent = new Intent(MainActivity.this, lingkaran.class);
+        startActivity(intent);
+    }
+
+}
